4423ba5d328cfeece6777bccc6ab75e6ba93fc43
kirkiki <robineau@chene.IDM.fr>
2017-08-11 17:09:36 +0200

    caca

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Nenozaure' });
+});
+router.get('/movies', function(req, res, next) {
+  res.render('films', {title: 'My movies'});
 });
 
 module.exports = router;
